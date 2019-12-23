03814118efc719d7237bf78816b2ebd8340fc5e8
GURKAN KISAOGLU <e2171726@ceng.metu.edu.tr>
2019-12-23 21:57:46 +0300

    refactoring

--- TP2/exp1/router3.py
+++ TP2/exp1/router3.py
@@ -11,15 +11,15 @@ def sender(hostIp_listen, hostPort_listen,hostIp_send, hostPort_send):
     s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     while 1:
-        dataFromD = s2.recvfrom(100)
-        s.sendto(dataFromD[0], (hostIp_send, hostPort_send))   
+        dataFromD = s.recvfrom(100)
+        s2.sendto(dataFromD[0], (hostIp_send, hostPort_send))   
 
     s2.close()
     s.close()
 
 if __name__ == '__main__':
-	sListener = threading.Thread(target=listener, args=("10.10.3.2", 1043))
-    dListener = threading.Thread(target=listener, args=("10.10.3.2", 1043))
+	sListener = threading.Thread(target=sender, args=("10.10.3.2", 1043, "10.10.7.1", 3043))
+    dListener = threading.Thread(target=sender, args=("10.10.7.2", 3043, "10.10.3.1", 1043))
 
 	sListener.start()
     dListener.start()
