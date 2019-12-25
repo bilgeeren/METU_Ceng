eee12b7a85014d217c1eac342f7a0196f8b2fdc1
Bilge Eren <bilgeeren@de27.wls.metu.edu.tr>
2019-12-25 16:59:11 +0300

    anlamsiz

--- TP2/exp1/configureR3.sh
+++ TP2/exp1/configureR3.sh
@@ -7,8 +7,8 @@ d_adapter=$(ip route get $d | grep -Po '(?<=(dev )).*(?= src| proto)')
 sudo tc qdisc del dev $s_adapter root netem
 sudo tc qdisc del dev $d_adapter root netem
 
-sudo tc qdisc add dev $s_adapter root netem delay 50ms 5ms distribution normal
-sudo tc qdisc add dev $d_adapter root netem delay 50ms 5ms distribution normal
+sudo tc qdisc change dev $s_adapter root netem loss 5% delay 3ms
+sudo tc qdisc change dev $d_adapter root netem loss 5% delay 3ms
 
 
 
